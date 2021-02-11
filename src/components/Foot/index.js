0a3508a0d56e5bd0c9ebbb406404a632c75db8d5
Camila Alves Meyer <camila.alves85@gmail.com>
2021-02-10 20:58:09 -0800

    Created foot component

---
+++ src/components/Foot/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function Foot() {
+    return (
+        <div className="fixed-bottom nav-foot">
+            <p>©2021 Copyright: Camila Alves Meyer</p>
+        </div>
+    )
+}
+
+export default Foot;
