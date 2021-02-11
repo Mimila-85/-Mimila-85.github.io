f443d6b7cf9d4b2d11a0918ea5e1c759b182d3a0
Camila Alves Meyer <camila.alves85@gmail.com>
2021-02-10 21:10:08 -0800

    Created the routes, added navbar, and foot

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Project from "./pages/Project"
+import Contact from "./pages/Contact"
+import BadRequest from "./pages/BadRequest"
+import Navbar from "./components/Navbar"
+import Foot from "./components/Foot"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/about">
+            <About />
+          </Route>
+          <Route exact path="/project">
+            <Project />
+          </Route>
+          <Route exact path="/contact">
+            <Contact />
+          </Route>
+          <Route>
+            <BadRequest />
+          </Route>
+        </Switch>
+        <Foot />
+      </div>
+    </Router>
   );
 }
 
