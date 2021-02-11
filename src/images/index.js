280913a74f5c3e90e68b344acf7930a9cecdb1ea
Camila Alves Meyer <camila.alves85@gmail.com>
2021-02-10 21:01:10 -0800

    Created image index to handle icons images

---
+++ src/images/index.js
@@ -0,0 +1,10 @@
+export const BootSrap = require("./icons/bootStrap.png");
+export const Css = require("./icons/css.png");
+export const Express = require("./icons/express.png");
+export const GitHub = require("./icons/gitHub.png");
+export const Html = require("./icons/html.png");
+export const Js = require("./icons/js.png");
+export const MongoDB = require("./icons/mongoDB.png");
+export const MySQL = require("./icons/mySQL.png");
+export const NodeIcon = require("./icons/node.png");
+export const ReactIcon = require("./icons/react.png");
