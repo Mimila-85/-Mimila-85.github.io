055b474e25b708a07b9e84a9816940b28edf5738
Camila Alves Meyer <camila.alves85@gmail.com>
2021-02-13 05:43:51 -0800

    Modified Router to work with GitHub pages

--- src/App.js
+++ src/App.js
@@ -1,38 +1,36 @@
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import "./App.css"
-import Home from "./pages/Home"
-import About from "./pages/About"
-import Project from "./pages/Project"
-import Contact from "./pages/Contact"
-import BadRequest from "./pages/BadRequest"
-import Navbar from "./components/Navbar"
-import Foot from "./components/Foot"
+import { HashRouter, Route, Switch } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Project from "./pages/Project";
+import Contact from "./pages/Contact";
+import BadRequest from "./pages/BadRequest";
+import Navbar from "./components/Navbar";
+import Foot from "./components/Foot";
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Navbar />
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route exact path="/about">
-            <About />
-          </Route>
-          <Route exact path="/project">
-            <Project />
-          </Route>
-          <Route exact path="/contact">
-            <Contact />
-          </Route>
-          <Route>
-            <BadRequest />
-          </Route>
-        </Switch>
-        <Foot />
-      </div>
-    </Router>
+    <HashRouter>
+      <Navbar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/about">
+          <About />
+        </Route>
+        <Route exact path="/project">
+          <Project />
+        </Route>
+        <Route exact path="/contact">
+          <Contact />
+        </Route>
+        <Route>
+          <BadRequest />
+        </Route>
+      </Switch>
+      <Foot />
+    </HashRouter>
   );
 }
 
