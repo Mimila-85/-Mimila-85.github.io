3c16834a2334616d86b55c515e4b4bab0ab99165
Camila Alves Meyer <camila.alves85@gmail.com>
2021-03-01 09:26:58 -0800

    Modified content from card

--- src/components/CardText/index.js
+++ src/components/CardText/index.js
@@ -2,21 +2,17 @@ import React from "react";
 import "./style.css";
 
 function CardText(props) {
-    return (
-        <p className="card-text">
-            Technologies: {props.technologies}
-            <br />
-            <br />
-            {props.description}
-            <br />
-            <br />
-            {props.responsibility}
-            <br />
-            <br />
-            Check usage information at my GitHub ReadMe.
-        </p>
-
-    )
+  return (
+    <p className="card-text">
+      Technologies: {props.technologies}
+      <br />
+      <br />
+      {props.description}
+      <br />
+      <br />
+      Check usage information at my GitHub ReadMe.
+    </p>
+  );
 }
 
 export default CardText;
--- src/components/Cards/index.js
+++ src/components/Cards/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./style.css"
+import "./style.css";
 import CardBody from "../CardBody";
 import CardBtn from "../CardBtn";
 import CardImg from "../CardImg";
@@ -7,38 +7,32 @@ import CardText from "../CardText";
 import CardTitle from "../CardTitle";
 
 function Card(props) {
-    return (
-        <div className="row row-cols-1 row-cols-sm-3">
-            {props.projects.map((project, index) => (
-                <div className="col mb-4">
-                    <div className="flip-card">
-                        <div className="flip-card-inner" key={Math.floor(index + 1 * Math.random() * 100)}>
-                            <div className="flip-card-front">
-                                <CardImg
-                                    img={project.image}
-                                    title={project.title}
-                                />
-                                <CardTitle
-                                    title={project.title}
-                                />
-                            </div>
-                            <CardBody>
-                                <CardText
-                                    technologies={project.technologies}
-                                    description={project.description}
-                                    responsibility={project.responsibility}
-                                />
-                                <CardBtn
-                                    livePage={project.livePage}
-                                    gitHub={project.gitHub}
-                                />
-                            </CardBody>
-                        </div>
-                    </div>
-                </div>
-            ))}
+  return (
+    <div className="row row-cols-1 row-cols-sm-3">
+      {props.projects.map((project, index) => (
+        <div className="col mb-4">
+          <div className="flip-card">
+            <div
+              className="flip-card-inner"
+              key={Math.floor(index + 1 * Math.random() * 100)}
+            >
+              <div className="flip-card-front">
+                <CardImg img={project.image} title={project.title} />
+                <CardTitle title={project.title} />
+              </div>
+              <CardBody>
+                <CardText
+                  technologies={project.technologies}
+                  description={project.description}
+                />
+                <CardBtn livePage={project.livePage} gitHub={project.gitHub} />
+              </CardBody>
+            </div>
+          </div>
         </div>
-    )
+      ))}
+    </div>
+  );
 }
 
 export default Card;
